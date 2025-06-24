e5050eece3117eb9bc5dc1a815ade49be8a2f9d4
soochan <audtncks77@gmail.com>
2025-06-24 16:52:57 +0900

    [Refactor] BaseViewModel MutableState > MutableStateFlow

--- core/android/src/main/java/com/chan/android/BaseViewModel.kt
+++ core/android/src/main/java/com/chan/android/BaseViewModel.kt
@@ -1,12 +1,11 @@
 package com.chan.android
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
@@ -20,12 +19,10 @@ abstract class BaseViewModel<Event : ViewEvent, UiState : ViewState, Effect : Vi
     abstract fun setInitialState(): UiState
     abstract fun handleEvent(event: Event)
 
-    private val initialState: UiState by lazy { setInitialState() }
-
     private val _event: MutableSharedFlow<Event> = MutableSharedFlow()
 
-    private val _viewState: MutableState<UiState> = mutableStateOf(initialState)
-    val viewState: State<UiState> = _viewState
+    private val _viewState = MutableStateFlow<UiState>(setInitialState())
+    val viewState: StateFlow<UiState> = _viewState
 
     private val _effect: Channel<Effect> = Channel()
     val effect = _effect.receiveAsFlow()
