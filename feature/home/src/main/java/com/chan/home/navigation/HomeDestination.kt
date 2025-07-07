e4b2ac9578bd6338f54ebb978eaf1148fdb1a472
soochan <audtncks77@naver.com>
2025-07-07 11:25:23 +0900

    Refactor: BottomNavDestination 수정에 따른, feature Destination 수정

--- feature/home/src/main/java/com/chan/home/navigation/HomeDestination.kt
+++ feature/home/src/main/java/com/chan/home/navigation/HomeDestination.kt
@@ -4,10 +4,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.navigation.NamedNavArgument
 import com.chan.home.R
-import com.chan.navigation.NavDestination
+import com.chan.navigation.BottomNavDestination
 import com.chan.navigation.Routes
 
-object HomeDestination : NavDestination {
+object HomeDestination : BottomNavDestination {
     override val route = Routes.HOME.route
     override val title = R.string.home_tab_home
     override val icon = Icons.Default.Home
