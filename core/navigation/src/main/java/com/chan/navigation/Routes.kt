a6a0d5c7ac63c6f3e336e9be3e4194701571c94d
soochan <audtncks77@naver.com>
2025-07-07 11:22:43 +0900

    Feat: Routes 카데고리 상세 경로 추가

--- core/navigation/src/main/java/com/chan/navigation/Routes.kt
+++ core/navigation/src/main/java/com/chan/navigation/Routes.kt
@@ -1,5 +1,12 @@
 package com.chan.navigation
 
 enum class Routes(val route: String) {
-    HOME("home"), CATEGORY("category"), HISTORY("history"), MYPAGE("mypage")
+    HOME("home"),
+    CATEGORY("category"),
+    CATEGORY_DETAIL("categoryDetail/{categoryId}"),
+    HISTORY("history"),
+    MYPAGE("mypage");
+
+    fun categoryDetailRoute(categoryId: String) =
+        "categoryDetail/$categoryId"
 }
