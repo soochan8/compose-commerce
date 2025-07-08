e43b7a86e79bf6bd500f9af96644fad09a24c908
soochan <audtncks77@naver.com>
2025-07-08 17:12:08 +0900

    Refactor: HomeScreen 상태 관리 분리
    상태 관리와 UI 컨텐츠로 분리

--- feature/home/src/main/java/com/chan/home/composables/HomeScreen.kt
+++ feature/home/src/main/java/com/chan/home/composables/HomeScreen.kt
@@ -1,24 +1,17 @@
 package com.chan.home.composables
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.chan.home.home.HomeContract
 import com.chan.home.home.HomeViewModel
 import com.chan.home.model.HomeTabItem
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(
@@ -31,8 +24,7 @@ fun HomeScreen(
         initialPage = 0,
         pageCount = { tabList.size }
     )
-
-    val homeCategoryRankingPagerState = rememberPagerState { state.rankingCategories.size }
+    val scope = rememberCoroutineScope()
 
     LaunchedEffect(Unit) {
         homeViewModel.setEvent(HomeContract.Event.BannerLoad)
@@ -41,41 +33,14 @@ fun HomeScreen(
         homeViewModel.setEvent(HomeContract.Event.SaleProducts)
     }
 
-    Scaffold(
-        topBar = { HomeTopTab(tabList = tabList, pagerState = pagerState) },
-        modifier = Modifier.fillMaxSize()
-    ) { innerPadding ->
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = innerPadding.calculateTopPadding())
-        ) { page ->
-            when (tabList[page]) {
-                HomeTabItem.Home -> {
-                    LazyColumn {
-                        item {
-                            HomeBanner(bannerList = state.bannerList)
-                            Spacer(modifier = Modifier.height(12.dp))
-
-                            HomePopularItemList(popularItem = state.popularItemList)
-                            Spacer(modifier = Modifier.height(12.dp))
-
-                            HomeCategoryRanking(
-                                categories = state.rankingCategories,
-                                pagerState = homeCategoryRankingPagerState
-                            )
-                            Spacer(modifier = Modifier.height(12.dp))
-
-                            HomeSaleProduct(saleProduct = state.saleProductList)
-                        }
-                    }
-                }
-
-                HomeTabItem.RecommendToday -> RecommendScreen()
-                // 나머지 탭 추후 구현
-                else -> {}
+    HomeScreenContent(
+        homeState = state,
+        tabs = tabList,
+        pagerState = pagerState,
+        onTabClick = { index ->
+            scope.launch {
+                pagerState.scrollToPage(index)
             }
         }
-    }
+    )
 }
