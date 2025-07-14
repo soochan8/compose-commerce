c7839e22ff124d1f76c0ac790bd823374ef0f591
soochan <audtncks77@naver.com>
2025-07-14 15:49:17 +0900

    refactor : 기존 상품카테고리 랭킹, 세일 상품 수정

--- feature/home/src/main/java/com/chan/home/data/mapper/ProductEntityMapper.kt
+++ feature/home/src/main/java/com/chan/home/data/mapper/ProductEntityMapper.kt
@@ -1,39 +1,17 @@
 package com.chan.home.data.mapper
 
-import com.chan.database.entity.PriceEntity
+import com.chan.database.dto.CategoryTabDto
 import com.chan.database.entity.ProductEntity
 import com.chan.database.entity.home.HomeBannerEntity
-import com.chan.database.entity.home.HomeSaleProductEntity
-import com.chan.database.entity.ranking.RankingCategoryEntity
 import com.chan.home.domain.vo.HomeBannerVO
-import com.chan.home.domain.vo.HomeSaleProductVO
 import com.chan.home.domain.vo.ProductVO
-import com.chan.home.domain.vo.RankingCategoryVO
-import com.chan.home.domain.vo.common.PriceVO
+import com.chan.home.domain.vo.RankingCategoryTabVO
 
 fun HomeBannerEntity.toDomain(): HomeBannerVO {
     return HomeBannerVO(
         id = id,
         imageUrl = imageUrl,
         title = title
-    )
-}
-
-fun HomeSaleProductEntity.toDomain(): HomeSaleProductVO {
-    return HomeSaleProductVO(
-        id = id,
-        imageUrl = imageUrl,
-        productName = productName,
-        price = price.toDomain(),
-        tags = tags
-    )
-}
-
-fun PriceEntity.toDomain(): PriceVO {
-    return PriceVO(
-        discountPercent = discountPercent,
-        discountedPrice = discountedPrice,
-        originalPrice = originalPrice
     )
 }
 
@@ -52,30 +30,13 @@ fun ProductEntity.SubCategories.Products.toDomain(): ProductVO {
     )
 }
 
-fun RankingCategoryEntity.toDomain(): RankingCategoryVO {
-    return RankingCategoryVO(
-        rankingCategoryId = rankingCategoryId,
-        rankingCategoryName = rankingCategoryName,
-        rankingCategoryItems = rankingCategoryItems.map { it.toDomain() }
+fun CategoryTabDto.toDomain() : RankingCategoryTabVO {
+    return RankingCategoryTabVO(
+        id = id,
+        name = name
     )
 }
 
-fun RankingCategoryEntity.RankingCategoryItems.toDomain(): RankingCategoryVO.RankingCategoryItemsVO {
-    return RankingCategoryVO.RankingCategoryItemsVO(
-        imageUrl = imageUrl,
-        productName = productName,
-        price = price.toDomain(),
-        tags = tags,
-        isLiked = isLiked,
-        isInCart = isInCart
-    )
-}
 
-fun RankingCategoryEntity.RankingCategoryItems.PriceDto.toDomain(): RankingCategoryVO.RankingCategoryItemsVO.PriceVO {
-    return RankingCategoryVO.RankingCategoryItemsVO.PriceVO(
-        discountPercent = discountPercent,
-        discountedPrice = discountedPrice,
-        originalPrice = originalPrice
-    )
-}
+
 
