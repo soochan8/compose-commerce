de28e89ad4676515be5a912eea5d13a4964cfa94
soochan <audtncks77@naver.com>
2025-07-15 15:40:54 +0900

    remove: 기존에 사용하던 Category Dao, Mapper, Entity 등 제거

--- core/database/src/main/java/com/chan/database/Converters.kt
+++ core/database/src/main/java/com/chan/database/Converters.kt
@@ -2,10 +2,8 @@ package com.chan.database
 
 import androidx.room.TypeConverter
 import com.chan.database.entity.ProductEntity
-import com.chan.database.entity.ranking.RankingCategoryEntity.RankingCategoryItems
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import kotlin.text.isNullOrEmpty
 
 class Converters {
     private val gson = Gson()
@@ -21,28 +19,4 @@ class Converters {
             object :
                 TypeToken<List<ProductEntity.Categories>>() {}.type
         )
-
-    @TypeConverter
-    fun itemsToJson(items: List<RankingCategoryItems>): String =
-        gson.toJson(items)
-
-    @TypeConverter
-    fun jsonToItems(json: String): List<RankingCategoryItems> =
-        gson.fromJson(
-            json,
-            object :
-                TypeToken<List<RankingCategoryItems>>() {}.type
-        )
-
-    @TypeConverter
-    fun fromStringList(value: List<String>?): String {
-        return gson.toJson(value)
-    }
-
-    @TypeConverter
-    fun toStringList(value: String?): List<String>? {
-        if (value.isNullOrEmpty()) return emptyList()
-        val type = object : TypeToken<List<String>>() {}.type
-        return gson.fromJson(value, type)
-    }
 }
--- feature/category/src/main/java/com/chan/category/data/repositoryImpl/CategoryRepositoryImpl.kt
+++ feature/category/src/main/java/com/chan/category/data/repositoryImpl/CategoryRepositoryImpl.kt
@@ -1,12 +1,8 @@
 package com.chan.category.data.repositoryImpl
 
-import com.chan.category.data.datasource.LocalDataSource
-import com.chan.category.data.datasource.RemoteDataSource
 import com.chan.category.data.mapper.toCategoriesDto
-import com.chan.category.data.mapper.toDomain
 import com.chan.category.domain.CategoryRepository
 import com.chan.category.domain.dto.CategoriesDto
-import com.chan.category.domain.vo.CategoryVO
 import com.chan.database.dao.ProductDao
 import javax.inject.Inject
 
