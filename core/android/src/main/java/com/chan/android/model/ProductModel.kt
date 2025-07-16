1d35f57bf8bf69ce02eb797a91af99c6c870177f
soochan <audtncks77@naver.com>
2025-07-16 10:25:12 +0900

    refactor: 공통 ProductModel 구조 수정 및 ProductCard 네이밍 수정

--- core/android/src/main/java/com/chan/android/model/ProductModel.kt
+++ core/android/src/main/java/com/chan/android/model/ProductModel.kt
@@ -3,7 +3,12 @@ package com.chan.android.model
 data class ProductModel(
     val productId: String,
     val productName: String,
+    val brandName: String,
     val imageUrl: String,
-    val price: Price,
-    val review: Review?
+    val originalPrice: String,
+    val discountPercent: String,
+    val discountPrice: String,
+    val tags: List<String>,
+    val reviewRating: Float,
+    val reviewCount: String
 )
