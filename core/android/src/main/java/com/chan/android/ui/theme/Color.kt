7f8f12e38674fee4d8ed570ed922273511aa7d29
soochan <audtncks77@naver.com>
2025-07-16 16:14:27 +0900

    feat: 공통 디자인 시스템(Color, Dimen, Type) 정의

---
+++ core/android/src/main/java/com/chan/android/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.chan.android.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val LightGray = Color(0xFFCCCCCC)
+val Red = Color(0xFFFF0000)
+val Gray = Color(0xFF888888)
+
+val TagBoxColor = Color(0xFFF1F1F1)
+val TodayDeliveryTagColor = Color(0xFFC56692)
+val DefaultTagColor = Color(0xFF666666)
+
+val CategoryBackground = Color(0xFFF6F7F9)
---
+++ core/android/src/main/java/com/chan/android/ui/theme/Type.kt
@@ -0,0 +1,63 @@
+package com.chan.android.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+
+val LocalAppTypography = compositionLocalOf { AppTypography() }
+
+val MaterialTheme.appTypography: AppTypography
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalAppTypography.current
+
+data class AppTypography(
+    val title: TextStyle = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = FontSize.title,
+        color = Black
+    ),
+    val productTitle: TextStyle = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = FontSize.productTitle,
+        color = Black,
+        lineHeight = LineHeight.productTitleSpacing
+    ),
+    val originPrice: TextStyle = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = FontSize.originPrice,
+        color = LightGray,
+        textDecoration = TextDecoration.LineThrough
+    ),
+    val discountPrice: TextStyle = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = FontSize.discountPrice,
+        color = Red,
+    ),
+    val discountPercent: TextStyle = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = FontSize.discountPercent,
+        color = Black
+    ),
+    val tagLabel: TextStyle = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = FontSize.tag,
+    ),
+    val review: TextStyle = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = FontSize.review,
+        color = Gray
+    )
+)
