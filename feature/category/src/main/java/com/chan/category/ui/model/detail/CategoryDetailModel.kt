1e11735fdb4a97f6fd690d7f1d2ba6da6b155eaa
soochan <audtncks77@naver.com>
2025-07-16 10:24:11 +0900

    remove: 기존 CategoryDetailModel, VO 제거

--- feature/category/src/main/java/com/chan/category/ui/model/detail/CategoryDetailModel.kt
+++
@@ -1,9 +0,0 @@
-package com.chan.category.ui.model.detail
-
-data class CategoryDetailModel(
-    val productId: String,
-    val productName: String,
-    val imageUrl: String,
-    val price: PriceModel,
-    val review: ReviewModel?
-)
--- feature/category/src/main/java/com/chan/category/ui/model/detail/CategoryNamesModel.kt
+++
@@ -1,7 +0,0 @@
-package com.chan.category.ui.model.detail
-
-data class CategoryNamesModel(
-    val id: Int,
-    val name: String,
-    val sortOrder: Int
-)
