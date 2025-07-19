f2844e64998bb97ff5e2528b8e9f608738df1d2e
soochan <audtncks77@naver.com>
2025-07-19 19:20:56 +0900

    feat: 상품상세 경로 추가

--- core/navigation/src/main/java/com/chan/navigation/Routes.kt
+++ core/navigation/src/main/java/com/chan/navigation/Routes.kt
@@ -4,9 +4,13 @@ enum class Routes(val route: String) {
     HOME("home"),
     CATEGORY("category"),
     CATEGORY_DETAIL("categoryDetail/{categoryId}"),
+    PRODUCT_DETAIL("productDetail/{productId}"),
     HISTORY("history"),
     MYPAGE("mypage");
 
     fun categoryDetailRoute(categoryId: String) =
         "categoryDetail/$categoryId"
+
+    fun productDetailRoute(productId: String) =
+        "productDetail/$productId"
 }
