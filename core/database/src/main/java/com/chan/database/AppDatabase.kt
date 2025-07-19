a7c81cf4534a4a4a296e33a39225836c3379ca22
soochan <audtncks77@naver.com>
2025-07-19 22:16:08 +0900

    feat: 상품 상세 Dao 생성 및 추가

--- core/database/src/main/java/com/chan/database/AppDatabase.kt
+++ core/database/src/main/java/com/chan/database/AppDatabase.kt
@@ -5,16 +5,19 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.chan.database.dao.HomeBannerDao
 import com.chan.database.dao.ProductDao
+import com.chan.database.dao.ProductDetailDao
+import com.chan.database.entity.ProductDetailEntity
 import com.chan.database.entity.ProductEntity
 import com.chan.database.entity.home.HomeBannerEntity
 
 @Database(
-    entities = [HomeBannerEntity::class, ProductEntity::class],
-    version = 11
+    entities = [HomeBannerEntity::class, ProductEntity::class, ProductDetailEntity::class],
+    version = 12
 )
 
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun homeBannerDao(): HomeBannerDao
     abstract fun productDao(): ProductDao
+    abstract fun productDetailDao(): ProductDetailDao
 }
