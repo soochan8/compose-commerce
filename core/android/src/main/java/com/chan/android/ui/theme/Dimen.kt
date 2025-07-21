5153d701beb1fe0dbec6d55066e0ca63e165e690
soochan <audtncks77@naver.com>
2025-07-21 16:43:44 +0900

    feat : 공통 시스템 추가(Type, Dimen, Color)

--- core/android/src/main/java/com/chan/android/ui/theme/Dimen.kt
+++ core/android/src/main/java/com/chan/android/ui/theme/Dimen.kt
@@ -6,13 +6,23 @@ import androidx.compose.ui.unit.sp
 object FontSize {
     val title = 18.sp
     val productTitle = 12.sp
-
     val originPrice = 10.sp
     val discountPrice = 14.sp
     val discountPercent = 14.sp
-
     val tag = 9.sp
     val review = 10.sp
+
+    val productDetailName = 16.sp
+    val productDetailBrandName = 14.sp
+    val productDetailOriginPrice = 14.sp
+    val productDetailDiscount = 22.sp
+    val productDetailReview = 13.sp
+    val bottomBarTodayDelivery = 14.sp
+    val bottomBarButton = 15.sp
+    val purchaseQuantity = 12.sp
+    val purchasePriceLabel = 16.sp
+    val purchaseQuantityLabel = 14.sp
+    val purchaseTotalPrice = 16.sp
 }
 
 object LineHeight {
@@ -27,4 +37,13 @@ object Spacing {
     val spacing5 = 20.dp
     val spacing6 = 24.dp
     val spacing7 = 28.dp
+}
+
+object Radius {
+    val radius1 = 4.dp
+    val radius2 = 8.dp
+    val radius3 = 12.dp
+    val radius4 = 16.dp
+    val radius5 = 20.dp
+    val radius6 = 24.dp
 }
