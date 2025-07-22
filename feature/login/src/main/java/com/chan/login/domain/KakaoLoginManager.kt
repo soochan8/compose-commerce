c10ee1cee0a756a040535e5cda3c588737774283
soochan <audtncks77@naver.com>
2025-07-22 18:57:11 +0900

    feat: 카카오톡 로그인 로직 분리 및 구현

---
+++ feature/login/src/main/java/com/chan/login/domain/KakaoLoginManager.kt
@@ -0,0 +1,13 @@
+package com.chan.login.domain
+
+import kotlinx.coroutines.flow.Flow
+
+sealed class KakaoLoginResult {
+    data class Success(val accessToken: String) : KakaoLoginResult()
+    data class Error(val message: String) : KakaoLoginResult()
+    object Cancelled : KakaoLoginResult()
+}
+
+interface KakaoLoginManager {
+    fun login(): Flow<KakaoLoginResult>
+} 
