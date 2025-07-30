55a61d067861d39459179b00c1dffaffe1a60c93
soochan <audtncks77@naver.com>
2025-07-30 18:20:01 +0900

    feat: mapper 추가

--- feature/search/src/main/java/com/chan/search/data/mappers/SearchToDomainMapper.kt
+++ feature/search/src/main/java/com/chan/search/data/mappers/SearchToDomainMapper.kt
@@ -1,9 +1,12 @@
 package com.chan.search.data.mappers
 
 import com.chan.database.entity.ProductEntity
+import com.chan.database.entity.search.SearchHistoryEntity
 import com.chan.domain.ProductVO
+import com.chan.search.domain.model.SearchHistoryVO
+import com.chan.search.ui.model.SearchHistoryModel
 
-fun ProductEntity.Categories.SubCategories.Products.toDomain() : ProductVO {
+fun ProductEntity.Categories.SubCategories.Products.toDomain(): ProductVO {
     return ProductVO(
         productId = productId,
         productName = productName,
@@ -15,5 +18,19 @@ fun ProductEntity.Categories.SubCategories.Products.toDomain() : ProductVO {
         tags = tags,
         reviewRating = reviewRating,
         reviewCount = reviewCount
+    )
+}
+
+fun SearchHistoryEntity.toDomain(): SearchHistoryVO {
+    return SearchHistoryVO(
+        search = search,
+        timeStamp = timeStamp
+    )
+}
+
+fun SearchHistoryVO.toSearchHistoryEntity(): SearchHistoryEntity {
+    return SearchHistoryEntity(
+        search = search,
+        timeStamp = timeStamp
     )
 }
--- feature/search/src/main/java/com/chan/search/ui/mappers/SearchToPresentationMapper.kt
+++ feature/search/src/main/java/com/chan/search/ui/mappers/SearchToPresentationMapper.kt
@@ -1,11 +1,29 @@
 package com.chan.search.ui.mappers
 
 import com.chan.domain.ProductVO
+import com.chan.search.domain.model.SearchHistoryVO
+import com.chan.search.domain.model.TrendingSearchVO
+import com.chan.search.ui.model.SearchHistoryModel
 import com.chan.search.ui.model.SearchResultModel
+import com.chan.search.ui.model.TrendingSearchModel
 
 fun ProductVO.toSearchModel(): SearchResultModel {
     return SearchResultModel(
         productId = productId,
         productName = productName
+    )
+}
+
+fun SearchHistoryVO.toSearchHistoryModel() : SearchHistoryModel {
+    return SearchHistoryModel(
+        search = search
+    )
+}
+
+fun TrendingSearchVO.toTrendingSearchModel() : TrendingSearchModel {
+    return TrendingSearchModel(
+        rank = rank,
+        keyword = keyword,
+        change = change
     )
 }
