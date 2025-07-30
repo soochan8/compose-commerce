93d624b4bd0a4f98833057a08947030c24b81695
soochan <audtncks77@naver.com>
2025-07-30 18:18:51 +0900

    feat: 검색어 관련 Repository/RepositoryImpl 추가
    최근 검색어 가져오기, 검색어 추가, 검색어 삭제, 전체 삭제

--- feature/search/src/main/java/com/chan/search/data/repository/SearchRepositoryImpl.kt
+++ feature/search/src/main/java/com/chan/search/data/repository/SearchRepositoryImpl.kt
@@ -1,15 +1,43 @@
 package com.chan.search.data.repository
 
 import com.chan.database.dao.ProductDao
+import com.chan.database.dao.SearchHistoryDao
 import com.chan.domain.ProductVO
 import com.chan.search.data.mappers.toDomain
+import com.chan.search.data.mappers.toSearchHistoryEntity
+import com.chan.search.domain.model.SearchHistoryVO
 import com.chan.search.domain.repository.SearchRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class SearchRepositoryImpl @Inject constructor(
-    private val productDao: ProductDao
+    private val productDao: ProductDao,
+    private val searchHistoryDao: SearchHistoryDao
 ) : SearchRepository {
     override suspend fun searchProductName(search: String): List<ProductVO> {
         return productDao.searchProductsByName(search).map { it.toDomain() }
+    }
+
+
+    override fun getRecentSearches(): Flow<List<SearchHistoryVO>> {
+        return searchHistoryDao.getRecentSearches()
+            .map { entityList -> entityList.map { it.toDomain() } }
+    }
+
+    override suspend fun addSearch(search: String) {
+        val addSearch = SearchHistoryVO(
+            search = search,
+            timeStamp = System.currentTimeMillis()
+        ).toSearchHistoryEntity()
+        return searchHistoryDao.insertSearch(addSearch)
+    }
+
+    override suspend fun deleteSearch(search: String) {
+        return searchHistoryDao.deleteSearch(search)
+    }
+
+    override suspend fun clearAll() {
+        return searchHistoryDao.clearAll()
     }
 }
