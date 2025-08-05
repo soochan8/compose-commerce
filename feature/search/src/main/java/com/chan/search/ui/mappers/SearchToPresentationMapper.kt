79e80bce06a39758076fa3a179e35ed0d0cd633e
soochan <audtncks77@naver.com>
2025-08-05 17:59:56 +0900

    feat: 상품결과 화면 필터Model 및 Mapper 추가

--- feature/search/src/main/java/com/chan/search/ui/mappers/SearchToPresentationMapper.kt
+++ feature/search/src/main/java/com/chan/search/ui/mappers/SearchToPresentationMapper.kt
@@ -1,11 +1,14 @@
 package com.chan.search.ui.mappers
 
+import com.chan.android.model.ProductModel
 import com.chan.domain.ProductVO
 import com.chan.search.domain.model.SearchHistoryVO
 import com.chan.search.domain.model.TrendingSearchVO
 import com.chan.search.ui.model.SearchHistoryModel
 import com.chan.search.ui.model.SearchResultModel
 import com.chan.search.ui.model.TrendingSearchModel
+import java.text.NumberFormat
+import java.util.Locale
 
 fun ProductVO.toSearchModel(): SearchResultModel {
     return SearchResultModel(
@@ -25,5 +28,20 @@ fun TrendingSearchVO.toTrendingSearchModel() : TrendingSearchModel {
         rank = rank,
         keyword = keyword,
         change = change
+    )
+}
+
+fun ProductVO.toProductsModel(): ProductModel {
+    return ProductModel(
+        productId = productId,
+        productName = productName,
+        brandName = brandName,
+        imageUrl = "https://image.oliveyoung.co.kr/cfimages/cf-goods/uploads/images/thumbnails/550/10/0000/0016/A00000016559829ko.jpg?l=ko",
+        originalPrice = NumberFormat.getNumberInstance(Locale.KOREA).format(originalPrice) + "원",
+        discountPrice = NumberFormat.getNumberInstance(Locale.KOREA).format(discountPrice) + "원",
+        discountPercent = "${discountPercent}%",
+        tags = tags,
+        reviewRating = reviewRating,
+        reviewCount = "(${NumberFormat.getNumberInstance(Locale.KOREA).format(reviewCount)})"
     )
 }
