1219986aa849bb4e95e0fac293d8ea73bef520ed
soochan <audtncks77@naver.com>
2025-08-18 16:22:01 +0900

    feat: 필터 관련 이벤트 추가

--- feature/search/src/main/java/com/chan/search/ui/composables/SearchScreen.kt
+++ feature/search/src/main/java/com/chan/search/ui/composables/SearchScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.chan.search.ui.contract.SearchContract
 import com.chan.search.ui.viewmodel.SearchViewModel
 
 @Composable
@@ -16,7 +17,30 @@ fun SearchScreen(
 
     SearchScreenContent(
         navController = navController,
-        state = state,
-        onEvent = viewModel::setEvent
+        search = state.search,
+        recentSearches = state.recentSearches,
+        recommendedKeywords = state.recommendedKeywords,
+        trendingSearches = state.trendingSearches,
+        searchResults = state.searchResults,
+        currentTime = state.currentTime,
+        showSearchResult = state.showSearchResult,
+        searchResultProducts = state.searchResultProducts,
+        showFilter = state.showFilter,
+        selectedDeliveryOption = state.selectedDeliveryOption,
+        onSearchChanged = { viewModel.setEvent(SearchContract.Event.OnSearchChanged(it)) },
+        onClearSearch = { viewModel.setEvent(SearchContract.Event.OnClickClearSearch) },
+        onSearchClick = { viewModel.setEvent(SearchContract.Event.OnAddSearchKeyword(it)) },
+        onSearchTextFocus = { viewModel.setEvent(SearchContract.Event.OnSearchTextFocus) },
+        onRemoveSearchKeyword = { viewModel.setEvent(SearchContract.Event.OnRemoveSearchKeyword(it)) },
+        onClearAllRecentSearches = { viewModel.setEvent(SearchContract.Event.OnClearAllRecentSearches) },
+        onSearchResultItemClick = { viewModel.setEvent(SearchContract.Event.OnClickSearchProduct(it)) },
+        onUpdateFilterClick = { viewModel.setEvent(SearchContract.Event.OnUpdateFilterClick) },
+        onDeliveryOptionClick = { viewModel.setEvent(SearchContract.Event.OnDeliveryOptionChanged(it)) },
+        onClickBack = {
+            navController.popBackStack()
+        },
+        onClickCart = {
+            // TODO : Cart 로 이동
+        }
     )
 }
