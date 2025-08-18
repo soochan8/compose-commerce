17e49d7b861ae5b0c16c7ca50257eda6152ed38a
soochan <audtncks77@naver.com>
2025-08-18 16:23:26 +0900

    feat: 구조변경 및 AnimatedVisibility 적용
    필터 open/close 여부에 따라 AnimatedVisibility 적용

--- feature/search/src/main/java/com/chan/search/ui/composables/SearchScreenContent.kt
+++ feature/search/src/main/java/com/chan/search/ui/composables/SearchScreenContent.kt
@@ -1,8 +1,18 @@
 package com.chan.search.ui.composables
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,90 +25,166 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.chan.android.model.ProductModel
 import com.chan.android.ui.theme.Spacing
 import com.chan.android.ui.theme.White
 import com.chan.android.ui.theme.appTypography
 import com.chan.android.ui.theme.dividerColor
+import com.chan.android.ui.theme.Black
+import com.chan.navigation.Routes
 import com.chan.search.R
 import com.chan.search.ui.composables.result.SearchResultScreen
 import com.chan.search.ui.model.SearchHistoryModel
 import com.chan.search.ui.model.SearchResultModel
 import com.chan.search.ui.model.TrendingSearchModel
+import com.chan.search.ui.model.filter.DeliveryOption
 
 @Composable
 fun SearchScreenContent(
+    navController: NavHostController,
     search: String,
     recentSearches: List<SearchHistoryModel>,
     recommendedKeywords: List<String>,
     trendingSearches: List<TrendingSearchModel>,
     searchResults: List<SearchResultModel>,
     currentTime: String,
-    showSearchResult : Boolean,
-    searchResultProducts : List<ProductModel>,
+    showSearchResult: Boolean,
+    searchResultProducts: List<ProductModel>,
+    showFilter: Boolean,
+    selectedDeliveryOption: DeliveryOption?,
     onSearchChanged: (String) -> Unit,
     onClearSearch: () -> Unit,
-    onSearchClick: () -> Unit,
+    onSearchClick: (String) -> Unit,
+    onSearchTextFocus: () -> Unit,
     onRemoveSearchKeyword: (String) -> Unit,
     onClearAllRecentSearches: () -> Unit,
     onSearchResultItemClick: (String) -> Unit,
     onClickBack: () -> Unit,
     onClickCart: () -> Unit,
+    onUpdateFilterClick: () -> Unit,
+    onDeliveryOptionClick: (DeliveryOption) -> Unit
 ) {
-    Scaffold(
-        containerColor = White,
-        topBar = {
-            SearchTopAppBar(
-                onClickBack = onClickBack,
-                onClickCart = onClickCart
-            )
-        },
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Scaffold(
+            containerColor = White,
+            topBar = {
+                SearchTopAppBar(
+                    onClickBack = onClickBack,
+                    onClickCart = onClickCart
+                )
+            },
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize()
+            ) {
 
-            SearchTextField(
-                search = search,
-                onSearchChanged = onSearchChanged,
-                onClearSearch = onClearSearch,
-                onSearchClick = onSearchClick,
-                modifier = Modifier.padding(Spacing.spacing4)
-            )
-            HorizontalDivider(color = dividerColor, thickness = 1.dp)
+                SearchTextField(
+                    search = search,
+                    onSearchChanged = onSearchChanged,
+                    onClearSearch = onClearSearch,
+                    onSearchClick = {
+                        onSearchClick(it)
+                    },
+                    onSearchTextFocus = {
+                        onSearchTextFocus()
+                    },
+                    modifier = Modifier.padding(Spacing.spacing4)
+                )
+                HorizontalDivider(color = dividerColor, thickness = 1.dp)
 
-            if(!showSearchResult) {
-                if (search.isBlank()) {
-                    if (recentSearches.isNotEmpty()) {
-                        RecentSearchList(
-                            recentSearches = recentSearches,
-                            onRemoveSearch = onRemoveSearchKeyword,
-                            onClearAllRecentSearches = onClearAllRecentSearches,
+                if (!showSearchResult) {
+                    if (search.isBlank()) {
+                        if (recentSearches.isNotEmpty()) {
+                            RecentSearchList(
+                                recentSearches = recentSearches,
+                                onRemoveSearch = onRemoveSearchKeyword,
+                                onClearAllRecentSearches = onClearAllRecentSearches,
+                                onSearchClick = {
+                                    onSearchClick(it)
+                                },
+                            )
+                        }
+                        RecommendedKeywordList(recommendedKeywords = recommendedKeywords)
+                        TrendingSearchList(
+                            trendingSearches = trendingSearches,
+                            currentTime = currentTime
+                        )
+                    } else {
+                        SearchResultList(
+                            results = searchResults,
+                            searchQuery = search,
+                            onSearchResultItemClick = onSearchResultItemClick
                         )
                     }
-                    RecommendedKeywordList(recommendedKeywords = recommendedKeywords)
-                    TrendingSearchList(
-                        trendingSearches = trendingSearches,
-                        currentTime = currentTime
-                    )
                 } else {
-                    SearchResultList(
-                        results = searchResults,
-                        searchQuery = search,
-                        onSearchResultItemClick = onSearchResultItemClick
-                    )
+                    if (searchResultProducts.isEmpty()) {
+                        Text(text = stringResource(R.string.search_empty_product))
+                    } else {
+                        Box(modifier = Modifier.weight(1f)) {
+                            SearchResultScreen(
+                                products = searchResultProducts,
+                                onNavigateToFilter = onUpdateFilterClick,
+                                onProductClick = { productId ->
+                                    navController.navigate(
+                                        Routes.PRODUCT_DETAIL.productDetailRoute(productId)
+                                    )
+                                }
+                            )
+                        }
+                    }
                 }
-            } else {
-                SearchResultScreen(products = searchResultProducts) {
+            }
+        }
 
-                }
+        AnimatedVisibility(
+            visible = showFilter,
+            enter = fadeIn(animationSpec = tween(300)),
+            exit = fadeOut(animationSpec = tween(300))
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Black.copy(alpha = 0.5f))
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = onUpdateFilterClick
+                    )
+            )
+        }
+
+        AnimatedVisibility(
+            visible = showFilter,
+            enter = slideInHorizontally(
+                initialOffsetX = { it },
+                animationSpec = tween(300)
+            ),
+            exit = slideOutHorizontally(
+                targetOffsetX = { it },
+                animationSpec = tween(300)
+            )
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                SearchFilterScreen(
+                    selectedDeliveryOption = selectedDeliveryOption,
+                    onClose = onUpdateFilterClick,
+                    onDeliveryOptionClick = onDeliveryOptionClick,
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .fillMaxHeight()
+                )
             }
         }
     }
