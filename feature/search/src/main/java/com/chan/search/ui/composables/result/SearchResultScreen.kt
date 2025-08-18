dba8c747cc9f5707363e672bee9b5b0d0d6b220c
soochan <audtncks77@naver.com>
2025-08-18 16:24:45 +0900

    feat: 검색 후 보여지는 화면 추가
    공통 ProductCard를 통해 리스트 노출

--- feature/search/src/main/java/com/chan/search/ui/composables/result/SearchResultScreen.kt
+++ feature/search/src/main/java/com/chan/search/ui/composables/result/SearchResultScreen.kt
@@ -1,18 +1,32 @@
 package com.chan.search.ui.composables.result
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.chan.android.model.ProductModel
 import com.chan.search.ui.contract.SearchContract
+import com.chan.search.ui.viewmodel.SearchViewModel
 
 @Composable
 fun SearchResultScreen(
-    state: SearchContract.State,
-    onEvent: (SearchContract.Event) -> Unit,
+    viewModel: SearchViewModel = hiltViewModel(),
+    products: List<ProductModel>,
+    onNavigateToFilter: () -> Unit,
     onProductClick: (String) -> Unit
 ) {
+    val state by viewModel.viewState.collectAsState()
+
 
     SearchResultScreenContent(
-        state = state,
-        onEvent = onEvent,
+        products = products,
+        filters = state.filterChips,
+        onToggleFilter = {
+            viewModel.setEvent(SearchContract.Event.OnFilterChipClicked(it))
+        },
+        onNavigateToFilter = {
+            onNavigateToFilter()
+        },
         onProductClick = { productId ->
             onProductClick(productId)
         }
