8b6ee9db9811bac4bac4221cd8d7dda83b637f46
soochan <audtncks77@naver.com>
2025-08-19 16:43:50 +0900

    refactor: searchScreen/Content, searchResultScreen/Content 구조 개선
    - state, event 단일 전달 구조로 단순화
    - 파라피터 문제 개선 및 유지보수 향상

--- feature/search/src/main/java/com/chan/search/ui/composables/result/SearchResultScreen.kt
+++ feature/search/src/main/java/com/chan/search/ui/composables/result/SearchResultScreen.kt
@@ -1,31 +1,23 @@
 package com.chan.search.ui.composables.result
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.chan.android.model.ProductModel
 import com.chan.search.ui.contract.SearchContract
-import com.chan.search.ui.viewmodel.SearchViewModel
 
 @Composable
 fun SearchResultScreen(
-    viewModel: SearchViewModel = hiltViewModel(),
-    products: List<ProductModel>,
-    onNavigateToFilter: () -> Unit,
+    state: SearchContract.State,
+    onEvent: (SearchContract.Event) -> Unit,
     onProductClick: (String) -> Unit
 ) {
-    val state by viewModel.viewState.collectAsState()
-
 
     SearchResultScreenContent(
-        products = products,
+        products = state.searchResultProducts,
         filters = state.filterChips,
         onToggleFilter = {
-            viewModel.setEvent(SearchContract.Event.OnFilterChipClicked(it))
+            onEvent(SearchContract.Event.OnFilterChipClicked(it))
         },
         onNavigateToFilter = {
-            onNavigateToFilter()
+            onEvent(SearchContract.Event.OnUpdateFilterClick)
         },
         onProductClick = { productId ->
             onProductClick(productId)
