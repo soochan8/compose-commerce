9407a06ff9a72b24749769fde059a7e8b4507d3b
soochan <audtncks77@naver.com>
2025-08-22 18:17:47 +0900

    feat: CategoryDetailRepository/Impl 수정
    기존 productDao에서 getAll()로 가져와서 category 필터링
    수정 후, categoryDao에서 파라미터를 통해 가져오기

--- feature/category/src/main/java/com/chan/category/data/repositoryImpl/CategoryDetailRepositoryImpl.kt
+++ feature/category/src/main/java/com/chan/category/data/repositoryImpl/CategoryDetailRepositoryImpl.kt
@@ -1,22 +1,28 @@
 package com.chan.category.data.repositoryImpl
 
-import com.chan.category.data.mapper.toDomain
+import com.chan.category.data.mapper.toProductsVO
 import com.chan.category.data.mapper.toTabsDomain
 import com.chan.category.domain.CategoryDetailRepository
-import com.chan.category.domain.vo.ProductVO
 import com.chan.category.domain.vo.detail.CategoryDetailTabsVO
-import com.chan.database.dao.ProductDao
+import com.chan.database.dao.CategoryDao
+import com.chan.database.dao.ProductsDao
+import com.chan.domain.ProductsVO
 import javax.inject.Inject
 
 class CategoryDetailRepositoryImpl @Inject constructor(
-    private val productDao: ProductDao
+    private val productsDao: ProductsDao,
+    private val categoryDao: CategoryDao
 ) : CategoryDetailRepository {
-    override suspend fun getCategoryDetailTabs(subCategoryId: String): List<CategoryDetailTabsVO> {
-        return productDao.getSiblingSubCategories(subCategoryId).map { it.toTabsDomain() }
+
+    override suspend fun getCategoryTabs(categoryId: String): List<CategoryDetailTabsVO> {
+        return categoryDao.getParentCategory(categoryId).map { it.toTabsDomain() }
     }
 
-    override suspend fun getCategoryDetailProducts(subCategoryId: String): List<ProductVO> {
-        return productDao.getProductsBySubCategoryId(subCategoryId).map { it.toDomain() }
+    override suspend fun getProductsByCategory(categoryId: String): List<ProductsVO> {
+        return productsDao.getProductsByCategoryId(categoryId).map { it.toProductsVO() }
     }
 
+    override suspend fun getProductsAll(): List<ProductsVO> {
+        return productsDao.getAllProducts().map { it.toProductsVO() }
+    }
 }
