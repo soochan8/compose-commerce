20adfcdcb21e658721592873d7be604ee97c60b2
soochan <audtncks77@naver.com>
2025-08-22 18:10:47 +0900

    feat: ProductDao, CategoryDao 추가

---
+++ core/database/src/main/java/com/chan/database/dao/ProductsDao.kt
@@ -0,0 +1,32 @@
+package com.chan.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.chan.database.entity.CommonProductEntity
+
+@Dao
+interface ProductsDao {
+
+    @Query("SELECT * FROM products")
+    suspend fun getAllProducts(): List<CommonProductEntity>
+
+    /**
+     * categoryId가 "c1, c2, c3"형태로 구성
+     * 특정 categoryId를 찾기 위해 분리
+     */
+    @Query(
+        """
+    SELECT * FROM products
+    WHERE categoryIds LIKE :categoryId || ',%' 
+       OR categoryIds LIKE '%,' || :categoryId || ',%' 
+       OR categoryIds LIKE '%,' || :categoryId
+       OR categoryIds = :categoryId
+"""
+    )
+    suspend fun getProductsByCategoryId(categoryId: String): List<CommonProductEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllProducts(products: List<CommonProductEntity>)
+}
