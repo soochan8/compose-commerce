540f8a1b55887344f27a89b81cee0e1d4288bf81
soochan <audtncks77@naver.com>
2025-08-22 18:16:49 +0900

    feat: CategoryRepository/Impl 수정 및 remoteDataSource 추가
    기존 productDao에서 getAll()로 가져와서 category 필터링
    수정 후, categoryDao에서 가져오기

--- feature/category/src/main/java/com/chan/category/data/repositoryImpl/CategoryRepositoryImpl.kt
+++ feature/category/src/main/java/com/chan/category/data/repositoryImpl/CategoryRepositoryImpl.kt
@@ -1,15 +1,20 @@
 package com.chan.category.data.repositoryImpl
 
-import com.chan.category.data.mapper.toCategoriesDto
+import com.chan.category.data.datasource.CategoryRemoteDataSource
+import com.chan.category.data.mapper.toCategoryVO
 import com.chan.category.domain.CategoryRepository
-import com.chan.category.domain.dto.CategoriesDto
-import com.chan.database.dao.ProductDao
+import com.chan.category.domain.vo.CategoryVO
+import com.chan.database.dao.CategoryDao
 import javax.inject.Inject
 
 class CategoryRepositoryImpl @Inject constructor(
-    private val productDao: ProductDao
+    private val categoryDao: CategoryDao,
+    private val remoteDataSource: CategoryRemoteDataSource
 ) : CategoryRepository {
-    override suspend fun getCategories(): List<CategoriesDto> {
-        return productDao.getAll().map { it.toCategoriesDto() }
+    override suspend fun getAllCategories(): List<CategoryVO> {
+        if (categoryDao.getAllCategories().isEmpty()) {
+            categoryDao.insertAllCategories(remoteDataSource.loadCategoriesFromAsset())
+        }
+        return categoryDao.getAllCategories().map { it.toCategoryVO() }
     }
 }
