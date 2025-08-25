6e0e8a8cf76bb848d7a339529f65bb29eec89504
soochan <audtncks77@naver.com>
2025-08-25 17:55:56 +0900

    feat: 기존 아이디 찾기 제거 및 로그인 Repository/Impl 추가

--- feature/login/src/main/java/com/chan/login/data/repository/LoginRepositoryImpl.kt
+++ feature/login/src/main/java/com/chan/login/data/repository/LoginRepositoryImpl.kt
@@ -2,21 +2,53 @@ package com.chan.login.data.repository
 
 import com.chan.database.dao.UserDao
 import com.chan.login.data.mapper.toUserEntity
-import com.chan.login.data.mapper.toUserVO
 import com.chan.login.domain.repository.LoginRepository
 import com.chan.login.domain.vo.UserVO
+import com.chan.login.util.SecurityUtils
 import javax.inject.Inject
 
 class LoginRepositoryImpl @Inject constructor(
-    private val userDao: UserDao
+    private val userDao: UserDao,
 ) : LoginRepository {
-    override suspend fun registerUser(user: UserVO) {
-        userDao.insertUser(user.toUserEntity())
+    override suspend fun registerUser(
+        userId: String,
+        userPw: String
+    ): Result<Unit> {
+        return runCatching {
+            if (userDao.findByUsername(userId) != null) {
+                throw SecurityException("이미 존재하는 아이디입니다.")
+            }
+
+            val salt = SecurityUtils.generateSalt()
+            val hashedPassword = SecurityUtils.hashPassword(
+                password = userPw.toCharArray(),
+                salt = salt
+            )
+            val newUser = UserVO(
+                userId = userId,
+                hashedPassword = hashedPassword,
+                salt = salt
+            )
+
+            userDao.insertUser(newUser.toUserEntity())
+        }
+
     }
 
-    override suspend fun findUserByUserId(userId: String): UserVO? {
-        val user = userDao.findByUsername(userId)
+    override suspend fun appLogin(userId: String, userPw: String): Result<Unit> {
+        return runCatching {
+            val storedUser =
+                userDao.findByUsername(userId) ?: throw SecurityException("존재하지 않는 아이디입니다.")
 
-        return user?.toUserVO()
+            val isPasswordCorrect = SecurityUtils.verifyPassword(
+                password = userPw.toCharArray(),
+                salt = storedUser.salt,
+                hashedPassword = storedUser.hashedPassword
+            )
+
+            if (!isPasswordCorrect) throw SecurityException("비밀번호가 일치하지 않습니다.")
+
+            storedUser
+        }
     }
 }
