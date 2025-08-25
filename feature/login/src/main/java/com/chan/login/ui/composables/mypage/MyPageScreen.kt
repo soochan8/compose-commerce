b1da859e299b6a625722de29f041cccbb23e4991
soochan <audtncks77@naver.com>
2025-08-26 08:47:38 +0900

    feat: MyPage 모듈 생성

---
+++ feature/login/src/main/java/com/chan/login/ui/composables/mypage/MyPageScreen.kt
@@ -0,0 +1,9 @@
+package com.chan.login.ui.composables.mypage
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyPageScreen() {
+    Text(text = "마이 페이지 입니다")
+}
