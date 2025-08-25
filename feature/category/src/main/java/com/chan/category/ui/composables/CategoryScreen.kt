6dba20b2876fd25752fc6d453d63320648835cf1
soochan <audtncks77@naver.com>
2025-08-25 11:27:25 +0900

    feat: CategoryScreen composable 세분화

--- feature/category/src/main/java/com/chan/category/ui/composables/CategoryScreen.kt
+++ feature/category/src/main/java/com/chan/category/ui/composables/CategoryScreen.kt
@@ -1,41 +1,22 @@
 package com.chan.category.ui.composables
 
 import android.util.Log
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshotFlow
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import coil.compose.AsyncImage
-import com.chan.android.ui.theme.CategoryBackground
-import com.chan.android.ui.theme.Spacing
-import com.chan.android.ui.theme.appTypography
 import com.chan.category.ui.CategoryContract
 import com.chan.category.ui.CategoryViewModel
+import com.chan.category.ui.composables.category.CategoryContent
+import com.chan.category.ui.composables.category.CategorySidebar
 import com.chan.navigation.Routes
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
@@ -52,16 +33,22 @@ fun CategoryScreen(
     val state by categoryViewModel.viewState.collectAsState()
     val effects = categoryViewModel.effect
     val scope = rememberCoroutineScope()
-    val listState = rememberLazyListState()
-    val categoryState = rememberLazyListState()
+    val contentListState = rememberLazyListState()
+    val sidebarListState = rememberLazyListState()
 
-    LaunchedEffect(effects) {
-        effects.collect { effect ->
+    LaunchedEffect(Unit) {
+        categoryViewModel.effect.collect { effect ->
             when (effect) {
                 is CategoryContract.Effect.ShowError -> Log.d(
                     "CategoryScreen",
                     " Error : ${effect.errorMessage}"
                 )
+
+                is CategoryContract.Effect.Navigation.ToCategoryDetail -> {
+                    navController.navigate(
+                        Routes.CATEGORY_DETAIL.categoryDetailRoute(effect.categoryId)
+                    )
+                }
             }
         }
     }
@@ -69,12 +56,12 @@ fun CategoryScreen(
     LaunchedEffect(state.selectedCategoryId) {
         val index = state.categories.indexOfFirst { it.id == state.selectedCategoryId }
         if (index != -1) {
-            categoryState.animateScrollToItem(index)
+            sidebarListState.animateScrollToItem(index)
         }
     }
 
-    LaunchedEffect(listState) {
-        snapshotFlow { listState.firstVisibleItemIndex }
+    LaunchedEffect(contentListState) {
+        snapshotFlow { contentListState.firstVisibleItemIndex }
             .map { firstIndex ->
                 // 현재 보이는 첫 아이템 기준으로 헤더 찾기
                 state.headerPositions
@@ -86,113 +73,47 @@ fun CategoryScreen(
             .distinctUntilChanged()
             .collect { newCatId ->
                 categoryViewModel.setEvent(
-                    CategoryContract.Event.SelectedCategory(newCatId)
+                    CategoryContract.Event.OnCategorySidebarClick(newCatId)
                 )
             }
     }
 
-
-
     Row(
         modifier = Modifier.fillMaxSize()
     ) {
-        LazyColumn(
-            state = categoryState,
-            modifier = Modifier
-                .fillMaxWidth(0.33f)
-                .fillMaxHeight()
-                .background(color = CategoryBackground)
-        ) {
-            items(items = state.categories, key = { it.id }) { category ->
-                val selected = category.id == state.selectedCategoryId
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(
-                            color = if (selected) Color.White else Color.Transparent,
-                        )
-                        .clickable {
-                            val targetIndex = state.headerPositions
-                                .firstOrNull { it.second == category.id }
-                                ?.first
+        CategorySidebar(
+            categories = state.categories,
+            selectedCategoryId = state.selectedCategoryId,
+            listState = sidebarListState,
+            onCategoryClick = { categoryId ->
+                val targetIndex = state.headerPositions
+                    .firstOrNull { it.second == categoryId }
+                    ?.first
 
-                            if (targetIndex != null) {
-                                scope.launch {
-                                    listState.scrollToItem(
-                                        index = targetIndex,
-                                        scrollOffset = 0
-                                    )
-                                }
-                            }
-                            categoryViewModel.setEvent(
-                                CategoryContract.Event.SelectedCategory(
-                                    category.id
-                                )
-                            )
-                        }
-                        .padding(vertical = 2.dp, horizontal = Spacing.spacing2)
-                ) {
-                    Text(
-                        text = category.name,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = Spacing.spacing3, horizontal = Spacing.spacing2),
-                        style = MaterialTheme.appTypography.subCategoryTextStyle.copy(
-                            fontWeight = if (selected) FontWeight.Bold else FontWeight.Normal
-                        ),
-                        color = if (selected) Color.Black else Color.Gray
+                if (targetIndex != null) {
+                    scope.launch {
+                        contentListState.scrollToItem(
+                            index = targetIndex,
+                            scrollOffset = 0
+                        )
+                    }
+                }
+
+                categoryViewModel.setEvent(
+                    CategoryContract.Event.OnCategorySidebarClick(
+                        categoryId
                     )
-                }
+                )
             }
-        }
-        LazyColumn(
-            state = listState,
-            modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth()
-                .background(color = Color.White)
-                .padding(start = Spacing.spacing5)
-        ) {
-            state.categories.forEach { category ->
-                item(key = "parent-${category.id}") {
-                    Row(
-                        modifier = Modifier.padding(top = Spacing.spacing6),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        AsyncImage(
-                            model = category.imageUrl,
-                            contentDescription = "상위 카테고리 이미지",
-                            modifier = Modifier.size(Spacing.spacing7)
-                        )
-                        Spacer(modifier = Modifier.width(Spacing.spacing2))
-                        Text(
-                            text = category.name,
-                            style = MaterialTheme.appTypography.categoryTextStyle,
-                            modifier = Modifier
-                                .clickable {
-                                    navController.navigate(
-                                        Routes.CATEGORY_DETAIL.categoryDetailRoute(category.id)
-                                    )
-                                }
-                        )
-                    }
-                }
-                category.subCategories.forEach { subCategory ->
-                    item(key = "child-${subCategory.id}") {
-                        Text(
-                            text = subCategory.name,
-                            style = MaterialTheme.appTypography.subCategoryTextStyle,
-                            modifier = Modifier
-                                .padding(top = Spacing.spacing6)
-                                .clickable {
-                                    navController.navigate(
-                                        Routes.CATEGORY_DETAIL.categoryDetailRoute(subCategory.id)
-                                    )
-                                }
-                        )
-                    }
-                }
+        )
+        CategoryContent(
+            categories = state.categories,
+            state = contentListState,
+            onCategoryClick = { categoryId ->
+                categoryViewModel.setEvent(
+                    CategoryContract.Event.OnCategoryClick(categoryId)
+                )
             }
-        }
+        )
     }
 }
