b1f6f99ee747b3d0dd08df405bbf79e4237d6d2d
soochan <audtncks77@naver.com>
2025-08-25 17:54:56 +0900

    feat: Effect 관련 LaunchedEffect 추가

--- feature/login/src/main/java/com/chan/login/ui/composables/LoginScreen.kt
+++ feature/login/src/main/java/com/chan/login/ui/composables/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.chan.login.ui.composables
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,10 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -20,12 +23,24 @@ import com.chan.android.ui.theme.appTypography
 import com.chan.login.R
 import com.chan.login.ui.LoginContract
 import com.chan.login.ui.LoginViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun LoginScreen(
     viewModel: LoginViewModel = hiltViewModel()
 ) {
     val state by viewModel.viewState.collectAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        viewModel.effect.collectLatest { collect ->
+            when (collect) {
+                LoginContract.Effect.NavigateToHome -> {}
+                is LoginContract.Effect.ShowError -> Log.e("LoginErrorInfo", collect.errorMsg)
+            }
+        }
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
