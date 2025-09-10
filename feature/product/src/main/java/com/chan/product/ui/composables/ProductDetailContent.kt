8aeaf35238ac0ff14b10d360f5cf5bb8117edb23
soochan <audtncks77@naver.com>
2025-09-10 15:23:55 +0900

    feat: LaunchedEffect 추가 및 WebViewComponent 추가
    - webView 제어 로직

--- feature/product/src/main/java/com/chan/product/ui/composables/ProductDetailContent.kt
+++ feature/product/src/main/java/com/chan/product/ui/composables/ProductDetailContent.kt
@@ -1,9 +1,11 @@
 package com.chan.product.ui.composables
 
 import android.annotation.SuppressLint
+import android.util.Log
 import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
@@ -18,6 +20,8 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -25,16 +29,61 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.chan.android.ui.theme.Radius
 import com.chan.android.ui.theme.Spacing
+import com.chan.product.ui.ProductDetailContract
+import com.chan.product.ui.WebAppInterface
+import kotlinx.coroutines.flow.Flow
+
+
+private const val JAVASCRIPT_INTERFACE_NAME = "AndroidBridge"
 
 @OptIn(ExperimentalFoundationApi::class)
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-fun ProductDetailContent() {
+fun ProductDetailContent(
+    state: ProductDetailContract.State,
+    onEvent: (ProductDetailContract.Event) -> Unit,
+    effect: Flow<ProductDetailContract.Effect>
+) {
     var isExpanded by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    var currentToast by remember { mutableStateOf<Toast?>(null) }
+    var webView by remember { mutableStateOf<WebView?>(null) }
+
+    DisposableEffect(webView) {
+        onDispose {
+            webView?.destroy()
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        effect.collect { effect ->
+            when (effect) {
+                is ProductDetailContract.Effect.UpdateWebView -> {
+                    webView?.loadUrl("javascript:markCouponAsDownloaded('${effect.couponId}')")
+                }
+
+                is ProductDetailContract.Effect.RevertWebViewButton -> {
+                    webView?.loadUrl("javascript:revertCouponButton('${effect.couponId}')")
+                }
+
+                is ProductDetailContract.Effect.ShowToast -> {
+                    currentToast?.cancel()
+                    currentToast = Toast.makeText(context, effect.message, Toast.LENGTH_SHORT)
+                    currentToast?.show()
+                }
+
+                is ProductDetailContract.Effect.ShowError -> Log.d(
+                    "ProductDetailInfo",
+                    effect.message
+                )
+            }
+        }
+    }
 
     Column(
         modifier = Modifier
@@ -50,7 +99,11 @@ fun ProductDetailContent() {
                     } else {
                         Modifier.height(1000.dp)
                     }
-                )
+                ),
+            onWebViewReady = { webView = it },
+            onDownloadClick = { couponId ->
+                onEvent(ProductDetailContract.Event.OnCouponDownloadClick(couponId))
+            }
         )
         Surface(
             modifier = Modifier
@@ -69,7 +122,11 @@ fun ProductDetailContent() {
 
 @SuppressLint("SetJavaScriptEnabled")
 @Composable
-private fun WebViewComponent(modifier: Modifier = Modifier) {
+private fun WebViewComponent(
+    modifier: Modifier = Modifier,
+    onWebViewReady: (WebView) -> Unit,
+    onDownloadClick: (String) -> Unit
+) {
     val localProductDetailFilePath = "product_detail.html"
     AndroidView(
         modifier = modifier,
@@ -77,12 +134,15 @@ private fun WebViewComponent(modifier: Modifier = Modifier) {
             WebView(context).apply {
                 webViewClient = WebViewClient()
                 settings.javaScriptEnabled = true
-                settings.cacheMode = WebSettings.LOAD_DEFAULT
                 isVerticalScrollBarEnabled = true
+                settings.cacheMode = WebSettings.LOAD_DEFAULT
+                addJavascriptInterface(
+                    WebAppInterface(onDownloadClick),
+                    JAVASCRIPT_INTERFACE_NAME
+                )
+                loadUrl("file:///android_asset/$localProductDetailFilePath")
+                onWebViewReady(this)
             }
-        },
-        update = {
-            it.loadUrl("file:///android_asset/$localProductDetailFilePath")
         }
     )
 }
