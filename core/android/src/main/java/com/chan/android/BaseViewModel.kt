cbb98e755641e3c45d273d3ddcee5e7f37f89252
soochan <audtncks77@naver.com>
2025-09-14 12:59:56 +0900

    feat: remove import

--- core/android/src/main/java/com/chan/android/BaseViewModel.kt
+++ core/android/src/main/java/com/chan/android/BaseViewModel.kt
@@ -2,12 +2,10 @@ package com.chan.android
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 interface ViewEvent
