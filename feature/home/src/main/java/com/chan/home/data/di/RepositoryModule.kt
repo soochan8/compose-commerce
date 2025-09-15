124fd648961e04ed6bd61af8ea434b126d643232
soochan <audtncks77@naver.com>
2025-09-15 14:41:00 +0900

    feat: Add RankingRepository/UseCase Module

--- feature/home/src/main/java/com/chan/home/data/di/RepositoryModule.kt
+++ feature/home/src/main/java/com/chan/home/data/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.chan.home.data.di
 
 import com.chan.home.data.repository.HomeRepositoryImpl
+import com.chan.home.data.repository.RankingRepositoryImpl
 import com.chan.home.domain.repository.HomeRepository
+import com.chan.home.domain.repository.RankingRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -17,5 +19,11 @@ abstract class RepositoryModule {
     abstract fun bindHomeRepository(
         homeRepositoryImpl: HomeRepositoryImpl
     ): HomeRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindRankingRepository(
+        rankingRepositoryImpl: RankingRepositoryImpl
+    ): RankingRepository
 }
 
--- feature/home/src/main/java/com/chan/home/data/di/UseCaseModule.kt
+++ feature/home/src/main/java/com/chan/home/data/di/UseCaseModule.kt
@@ -1,13 +1,13 @@
 package com.chan.home.data.di
 
-import com.chan.home.data.repository.HomeRepositoryImpl
 import com.chan.home.domain.HomeBannerUseCase
 import com.chan.home.domain.HomeCategoryRankingProductsUseCase
 import com.chan.home.domain.HomeCategoryTabsUseCase
 import com.chan.home.domain.HomePopularProductsUseCase
 import com.chan.home.domain.HomeSaleProductsUseCase
 import com.chan.home.domain.HomeUseCases
-import com.chan.home.domain.repository.HomeRepository
+import com.chan.home.domain.usecase.RankingProductsUseCase
+import com.chan.home.domain.usecase.RankingUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -24,13 +24,22 @@ object UseCaseModule {
         homeSaleProductsUseCase: HomeSaleProductsUseCase,
         homeCategoryTabsUseCase: HomeCategoryTabsUseCase,
         homeCategoryRankingProductsUseCase: HomeCategoryRankingProductsUseCase
-    ) : HomeUseCases {
+    ): HomeUseCases {
         return HomeUseCases(
             homeBanner = homeBannerUseCase,
             popularProducts = homePopularProductsUseCase,
             saleProducts = homeSaleProductsUseCase,
             categoryTabs = homeCategoryTabsUseCase,
             categoryRankingProducts = homeCategoryRankingProductsUseCase
+        )
+    }
+
+    @Provides
+    fun provideRankingUseCases(
+        rankingProductsUseCase: RankingProductsUseCase
+    ): RankingUseCase {
+        return RankingUseCase(
+            rankingProductsUseCase = rankingProductsUseCase
         )
     }
 }
