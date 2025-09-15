dfdde34f1a893acd74da3be2dbe1b470bf92b4a7
soochan <audtncks77@naver.com>
2025-09-15 13:23:44 +0900

    feat: hilt 수정, 제거한 Repository DI 제거

--- feature/home/src/main/java/com/chan/home/data/di/RepositoryModule.kt
+++ feature/home/src/main/java/com/chan/home/data/di/RepositoryModule.kt
@@ -1,13 +1,7 @@
 package com.chan.home.data.di
 
-import com.chan.home.data.repository.HomeBannerRepositoryImpl
-import com.chan.home.data.repository.HomePopularItemRepositoryImpl
-import com.chan.home.data.repository.HomeSaleProductRepositoryImpl
-import com.chan.home.data.repository.RankingCategoryRepositoryImpl
-import com.chan.home.domain.repository.HomeBannerRepository
-import com.chan.home.domain.repository.HomePopularItemRepository
-import com.chan.home.domain.repository.HomeSaleProductRepository
-import com.chan.home.domain.repository.RankingCategoryRepository
+import com.chan.home.data.repository.HomeRepositoryImpl
+import com.chan.home.domain.repository.HomeRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -20,26 +14,8 @@ abstract class RepositoryModule {
 
     @Binds
     @Singleton
-    abstract fun bindHomeBannerRepository(
-        bannerRepositoryImpl: HomeBannerRepositoryImpl
-    ): HomeBannerRepository
-
-    @Binds
-    @Singleton
-    abstract fun bindHomePopularItemRepository(
-        bannerPopularItemRepositoryImpl: HomePopularItemRepositoryImpl
-    ): HomePopularItemRepository
-
-    @Binds
-    @Singleton
-    abstract fun bindRankingCategoryRepository(
-        rankingCategoryRepositoryImpl: RankingCategoryRepositoryImpl
-    ): RankingCategoryRepository
-
-    @Binds
-    @Singleton
-    abstract fun bindHomeSaleProductRepository(
-        homeSaleProductRepositoryImpl: HomeSaleProductRepositoryImpl
-    ): HomeSaleProductRepository
+    abstract fun bindHomeRepository(
+        homeRepositoryImpl: HomeRepositoryImpl
+    ): HomeRepository
 }
 
