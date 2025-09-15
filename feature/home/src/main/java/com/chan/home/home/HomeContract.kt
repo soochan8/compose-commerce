f6c60ed454a0ead9fbeb6a9b3794615cdd184376
soochan <audtncks77@naver.com>
2025-09-15 14:52:17 +0900

    feat: Ranking Event/State 추가

--- feature/home/src/main/java/com/chan/home/home/HomeContract.kt
+++ feature/home/src/main/java/com/chan/home/home/HomeContract.kt
@@ -1,5 +1,6 @@
 package com.chan.home.home
 
+import androidx.paging.PagingData
 import com.chan.android.ViewEffect
 import com.chan.android.ViewEvent
 import com.chan.android.ViewState
@@ -8,6 +9,7 @@ import com.chan.home.model.HomeBannerModel
 import com.chan.home.model.HomeRankingCategoryTabModel
 import com.chan.home.model.HomeSaleProductModel
 import com.chan.home.model.HomeTabItem
+import kotlinx.coroutines.flow.Flow
 
 class HomeContract {
     sealed class Event : ViewEvent {
@@ -22,10 +24,14 @@ class HomeContract {
         data class OnProductClicked(val productId: String) : Event()
         data class OnLikedClick(val productId: String) : Event()
         data class OnCartClicked(val productId: String) : Event()
+
+        sealed class HomeRankingEvent : ViewEvent {
+            object RankingProductsLoad : Event()
+        }
     }
 
     data class State(
-        val topBars : List<HomeTabItem> = emptyList(),
+        val topBars: List<HomeTabItem> = emptyList(),
         val bannerList: List<HomeBannerModel> = emptyList(),
         val popularProducts: List<ProductsModel> = emptyList(),
         val rankingCategoryTabs: List<HomeRankingCategoryTabModel> = emptyList(),
@@ -33,8 +39,15 @@ class HomeContract {
         val saleProductList: List<HomeSaleProductModel> = emptyList(),
         val selectedRankingTabIndex: Int = 0,
         val isLoading: Boolean = false,
-        val isError: Boolean = false
+        val isError: Boolean = false,
+
+        val homeRankingState: HomeRankingState = HomeRankingState()
     ) : ViewState
+
+
+    data class HomeRankingState(
+        val rankingProducts: Flow<PagingData<ProductsModel>>? = null
+    )
 
     sealed class Effect : ViewEffect {
         data class ShowError(val message: String) : Effect()
