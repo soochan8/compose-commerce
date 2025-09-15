cff7a027432c0c87dfe4752c90af6bb7daf339f8
soochan <audtncks77@naver.com>
2025-09-15 14:12:17 +0900

    chore: add paging3 compose dependency

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.storage)
     implementation(libs.androidx.paging)
+    implementation(libs.androidx.paging.compose)
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
