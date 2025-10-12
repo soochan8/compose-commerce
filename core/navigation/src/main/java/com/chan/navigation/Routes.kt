ac031dd5f7a592a1951b56056c364f3d779756f4
soochan <audtncks77@naver.com>
2025-10-12 17:10:32 +0900

    feat: 홈 배너 클릭 시 webView Route 추가

--- core/navigation/src/main/java/com/chan/navigation/Routes.kt
+++ core/navigation/src/main/java/com/chan/navigation/Routes.kt
@@ -1,7 +1,10 @@
 package com.chan.navigation
 
+import android.net.Uri
+
 enum class Routes(val route: String) {
     HOME("home"),
+    HOME_BANNER_WEB_VIEW("homeBannerWebView/{url}"),
     CATEGORY("category"),
     CATEGORY_DETAIL("categoryDetail/{categoryId}"),
     PRODUCT_DETAIL("productDetail/{productId}"),
@@ -12,6 +15,9 @@ enum class Routes(val route: String) {
     CART("cart"),
     CART_POPUP("cart/{productId}");
 
+    fun homeBannerWebViewRoute(url: String) =
+        "homeBannerWebView/${Uri.encode(url)}"
+
     fun categoryDetailRoute(categoryId: String) =
         "categoryDetail/$categoryId"
 
@@ -21,6 +27,7 @@ enum class Routes(val route: String) {
     fun cartPopUpRoute(productId: String) =
         "cart/$productId"
 }
+
 fun createLoginRoute(redirect: String = ""): String {
     return "${Routes.LOGIN.route}?redirect=$redirect"
 }
