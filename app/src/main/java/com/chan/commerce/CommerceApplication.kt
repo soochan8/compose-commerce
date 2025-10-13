90f4905354724a9c5ad7234acf1880833ee00443
soochan <audtncks77@naver.com>
2025-10-13 18:02:16 +0900

    feat: 앱 시작 시, NotificationChannel 추가

--- app/src/main/java/com/chan/commerce/CommerceApplication.kt
+++ app/src/main/java/com/chan/commerce/CommerceApplication.kt
@@ -1,13 +1,37 @@
 package com.chan.commerce
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class CommerceApplication : Application() {
+
+    companion object {
+        private const val CHANNEL_ID = "commerce_notification_channel"
+        private const val CHANNEL_NAME = "Commerce Notification"
+    }
+
+
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, getString(com.chan.login.R.string.kakao_native_app_key))
+
+        createNotificationChannels()
+    }
+
+    private fun createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
