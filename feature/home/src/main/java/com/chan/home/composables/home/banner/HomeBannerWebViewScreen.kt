69a73b7fac471a635607ca812666665935f137e7
soochan <audtncks77@naver.com>
2025-10-14 16:58:03 +0900

    feat: webView 메모리 누수 방지
    - AndroidView.factory에서 webView 생성 (외부 remember 제거)
    - onRelease 리소스 정리 추가

--- feature/home/src/main/java/com/chan/home/composables/home/banner/HomeBannerWebViewScreen.kt
+++ feature/home/src/main/java/com/chan/home/composables/home/banner/HomeBannerWebViewScreen.kt
@@ -5,13 +5,15 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import com.chan.home.home.HomeContract
 import kotlinx.coroutines.Dispatchers
@@ -28,32 +30,14 @@ fun HomeBannerWebViewScreen(
 ) {
     val updatedOnDownloadClick by rememberUpdatedState(onEvent)
     val scope = rememberCoroutineScope { Dispatchers.Main }
-    val context = LocalContext.current
-    val webView = remember(context) {
-        WebView(context).apply {
-            settings.javaScriptEnabled = true
-            settings.domStorageEnabled = true
-            webViewClient = WebViewClient()
-            webChromeClient = WebChromeClient()
-
-            addJavascriptInterface(
-                WebViewInterface(scope, updatedOnDownloadClick),
-                JAVASCRIPT_INTERFACE_NAME
-            )
-        }
-    }
-
-    LaunchedEffect(url) {
-        if (webView.url != url)
-            webView.loadUrl(url)
-    }
+    var webView by remember { mutableStateOf<WebView?>(null) }
 
     LaunchedEffect(Unit) {
         effect
             .filterIsInstance<HomeContract.Effect.Banner>()
             .collect { bannerEffect ->
                 when (bannerEffect) {
-                    HomeContract.Effect.Banner.ShowCouponDownloaded -> webView.evaluateJavascript(
+                    HomeContract.Effect.Banner.ShowCouponDownloaded -> webView?.evaluateJavascript(
                         "onCouponDownloadSuccess()",
                         null
                     )
@@ -62,7 +46,35 @@ fun HomeBannerWebViewScreen(
     }
 
     AndroidView(
-        factory = { webView },
+        factory = { context ->
+            WebView(context).apply {
+                webView = this
+
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                webViewClient = WebViewClient()
+                webChromeClient = WebChromeClient()
+
+                addJavascriptInterface(
+                    WebViewInterface(scope, updatedOnDownloadClick),
+                    JAVASCRIPT_INTERFACE_NAME
+                )
+
+                loadUrl(url)
+            }
+        },
+        update = {
+            if(it.url != url) it.loadUrl(url)
+        },
+        onRelease = {
+            it.apply {
+                stopLoading()
+                webChromeClient = null
+                removeJavascriptInterface(JAVASCRIPT_INTERFACE_NAME)
+                destroy()
+                webView = null
+            }
+        },
         modifier = Modifier.fillMaxSize()
     )
 }
