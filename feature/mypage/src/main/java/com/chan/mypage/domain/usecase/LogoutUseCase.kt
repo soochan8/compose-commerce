0b1eb90723e1f5994630170c2d742eeea6262a22
soochan <audtncks77@naver.com>
2025-10-22 15:48:54 +0900

    feat: 로그아웃 시, CartDataStoreManager 리소스 정리

---
+++ feature/mypage/src/main/java/com/chan/mypage/domain/usecase/LogoutUseCase.kt
@@ -0,0 +1,17 @@
+package com.chan.mypage.domain.usecase
+
+import android.content.Context
+import com.chan.auth.domain.AuthRepository
+import com.chan.database.datastore.CartDataStoreManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class LogoutUseCase @Inject constructor(
+    private val authRepository: AuthRepository,
+    @ApplicationContext private val context: Context
+) {
+    suspend operator fun invoke() {
+        authRepository.logout()
+        CartDataStoreManager.clearAll()
+    }
+}
