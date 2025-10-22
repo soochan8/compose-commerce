a9f6f2921df0b11d3ff255105bfcc3e90903d112
soochan <audtncks77@naver.com>
2025-10-22 15:25:01 +0900

    feat: dataStoreScope Module 설정
    - SupervisorJob + IO

---
+++ feature/cart/src/main/java/com/chan/cart/data/di/CoroutineScopeModule.kt
@@ -0,0 +1,19 @@
+package com.chan.cart.data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutineScopeModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStoreScope() : CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+}
